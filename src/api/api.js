60af61e38f1f054de88552abfe74b0a8d3466760
muj-convoc <140317929+muj-convoc@users.noreply.github.com>
2023-07-24 16:28:34 +0530

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 // const BASE_URL = "https://us-central1-muj-convocation.cloudfunctions.net/app";
 // const BASE_URL = "http://localhost:5000/muj-convocation/us-central1/app";
 const BASE_URL =
-  'https://us-central1-muj-convocation-2022.cloudfunctions.net/app';
+  'https://muj-convocation-2023.firebaseapp.com/';
 
 const API = axios.create({
   baseURL: BASE_URL,
